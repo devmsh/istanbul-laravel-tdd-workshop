ef58e37d27ebc088424ad8c57f9af91ac2727187
Mohammed S Shurrab <mshurrab@extreme.sa>
2020-03-11 19:17:40 +0300

    Install Laravel Dusk using `php artisan dusk:install` and update the laravel basic welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>TDD Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TDD Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.meetup.com/laravelistanbul/events/269059351/">Meetup</a>
+                    <a href="https://github.com/devmsh/istanbul-laravel-tdd-workshop">GitHub</a>
+                    <a href="https://laravel.com/docs/master/testing">Docs</a>
+                    <a href="https://laracasts.com/skills/testing">Laracasts</a>
+                </div>
+                <br/>
+                <div class="links">
+                    <a href="https://course.testdrivenlaravel.com">Test Driven Laravel</a>
+                    <a href="https://confidentlaravel.com">Confident Laravel</a>
+                    <a href="https://laravelshift.com/laravel-test-generator">Laravel Test Generator</a>
                 </div>
             </div>
         </div>
